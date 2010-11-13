7cd425a38824d6855571c4a772e446edc5349212
David Radcliffe <radcliffe.david@gmail.com>
2010-11-13 07:58:50 -0800

    changed extension info

--- homepager_extension.rb
+++ homepager_extension.rb
@@ -1,16 +1,7 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class HomepagerExtension < Spree::Extension
-  version "1.0"
+  version "1.1"
   description "Spree extension to create a homepage with some 'featured' products."
-  url "http://github.com/pero-ict/spree-homepager-extension"
-
-  # Please use homepager/config/routes.rb instead for extension routes.
-
-  # def self.require_gems(config)
-  #   config.gem "gemname-goes-here", :version => '1.2.3'
-  # end
+  url "http://github.com/dwradcliffe/spree-homepager"
   
   def activate
     
