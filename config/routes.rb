669113eae6e64c0ce17777f45439d52bc865ef61
Pedro Menezes <pedrojudo@gmail.com>
2011-06-20 14:14:00 -0300

    prevent routing to home#index when user searches something or tries to paginate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root :controller => "home", :action => "index"
+  root :to => 'home#index', :constraints => lambda { |request|
+    not (request.params.include?(:keywords) or request.params.include?(:page))
+  }
 end
